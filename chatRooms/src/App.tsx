a87fc7f6deded41ec5bbf963a9a9cace01970e97
Adiri Oghenetega <tegaadiri3@yahoo.com>
2023-02-16 11:13:18 -0600

    updated

--- chatRooms/src/App.tsx
+++ chatRooms/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 
 
   return (
-    <div className="bg-gray-500 h-[88vh] ">
+    <div className="bg-gray-500 h-[87vh] sm:h-[100vh] ">
 <Routes>
 <Route  path='/' element={<Login />} />
 <Route  path='/chats' element={<Chats socket={socket} />} />
